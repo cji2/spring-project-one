263a24e7fffa64d6150acc3d7b833700ad8af6a9
Changhoon Ji <cji2@gmu.edu>
2019-12-31 21:19:59 -0500

    create main method, which calls the getDailyWorkout() of BaseballCoach.

---
+++ spring-project-one/src/edu/gmu/springproj/MyApp.java
@@ -0,0 +1,14 @@
+package edu.gmu.springproj;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		// create the object.
+		BaseballCoach aCoach = new BaseballCoach();
+		
+		// use the object.		
+		String result = aCoach.getDailyWorkout();
+		System.out.println(result);
+	}
+}
