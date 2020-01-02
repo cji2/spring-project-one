a3113bbca92b257fc7f20f532d1e960dcb01510f
Changhoon Ji <cji2@gmu.edu>
2020-01-01 19:32:33 -0500

    define new implementation of Coach interface

---
+++ spring-project-one/src/edu/gmu/springproj/BibleCoach.java
@@ -0,0 +1,9 @@
+package edu.gmu.springproj;
+
+public class BibleCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on reading the Bible!";
+	}
+}
