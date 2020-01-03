3f60a3363abc918fe838905c99920912d0adab76
Changhoon Ji <cji2@gmu.edu>
2020-01-03 14:48:29 -0500

    modify main method to call getter for injected literal values.

--- spring-project-one/src/edu/gmu/springproj/SetterInjecApp.java
+++ spring-project-one/src/edu/gmu/springproj/SetterInjecApp.java
@@ -12,14 +12,20 @@ public class SetterInjecApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		/* retrieve bean from spring container.
-		   'myCoach' is bean id. 'Coach.class' is interface. */
-		Coach aCoach = context.getBean("myCricketCoach", Coach.class);
+		   'myCoach' is bean id. 'Coach.class' is interface. 
+		   we use CricketCoach object, rather than Coach,
+		   since Coach interface has neither emailAddress nor team. */
+		CricketCoach aCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		// call methods on the bean.
 		System.out.println(aCoach.getDailyWorkout());
 		
 		// call new method for fortune service.
 		System.out.println(aCoach.getDailyFortune());
+		
+		// call our new methods to get the injected literal values.
+		System.out.println(aCoach.getEmailAddress());
+		System.out.println(aCoach.getTeam());
 		
 		// close the application context.
 		context.close();
